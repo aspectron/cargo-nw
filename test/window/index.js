551c2851dd387fd01c6edfd6b69a70dbead3d3ee
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2022-12-20 19:10:48 -0500

    wip basic tests

--- test/window/index.js
+++ test/window/index.js
@@ -1,15 +1,15 @@
 nw.Window.open('index.html', {
     //new_instance: true,
     id: 'main',
-    title: 'NWJS Example',
+    title: 'NW Window Test',
     width: 1027,
     height: 768,
     resizable: true,
     frame: true,
     transparent: false,
     show: true,
-    // icon: 'resources/images/kdx-icon.png'
+    icon: 'icons/application.png'
     // http://docs.nwjs.io/en/latest/References/Manifest%20Format/#window-subfields
 }, (win) => {
-    console.log("win", win)
+    console.log("window", win)
 });
